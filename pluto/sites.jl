1a19311c914a72021a15feb961c451db6cd44516
Neel Smith <nsmith@Technites.local>
2021-03-06 07:11:25 -0500

    Plot points

--- pluto/sites.jl
+++ pluto/sites.jl
@@ -42,6 +42,19 @@ md"> Functions to manipulate and plot data"
 # ╔═╡ 4db92e1e-7e73-11eb-1f5b-31c82d374367
 md"> Data sets"
 
+# ╔═╡ 6ed4d0f2-7e74-11eb-3ab4-cf6b6f9f9165
+repo = dirname(pwd())
+
+# ╔═╡ c3fcc170-7e74-11eb-2b2f-eddca7c8ee14
+capitals = dataset("us-state-capitals")
+
+# ╔═╡ c801c23e-7e74-11eb-3b83-6f05f1dd46e8
+typeof(capitals)
+
+# ╔═╡ 41241d02-7e74-11eb-083c-1b00e60e3985
+lls = CSV.File(repo * "/data/simple-lls.cex", skipto
+=2, delim="|") |> DataFrame
+
 # ╔═╡ a5d80b70-7e72-11eb-364b-d54f4473e4e1
 world110m = dataset("world-110m")
 
@@ -66,7 +79,21 @@ function plotall()
                 feature=:land
             }
         }
-    )
+    ) + 
+	@vlplot(
+    :circle,
+    data=capitals,
+    longitude="lon:q",
+    latitude="lat:q",
+    color={value=:orange}
+) + 
+	@vlplot(
+    :circle,
+    data=lls,
+    longitude="lon:q",
+    latitude="lat:q",
+    color={value=:orange}
+)
 end
 
 # ╔═╡ b154a4ae-7e72-11eb-3239-517878a3a6fb
@@ -81,4 +108,8 @@ plotall()
 # ╟─768fd7b6-7e73-11eb-1826-d300c0ac0361
 # ╠═ae8aed3c-7e72-11eb-2004-3f41ad407fdd
 # ╟─4db92e1e-7e73-11eb-1f5b-31c82d374367
+# ╠═6ed4d0f2-7e74-11eb-3ab4-cf6b6f9f9165
+# ╠═c3fcc170-7e74-11eb-2b2f-eddca7c8ee14
+# ╠═c801c23e-7e74-11eb-3b83-6f05f1dd46e8
+# ╟─41241d02-7e74-11eb-083c-1b00e60e3985
 # ╟─a5d80b70-7e72-11eb-364b-d54f4473e4e1
